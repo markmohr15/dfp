3d7b411d2d9107fea46b408963f70565f10350b8
markmohr15 <mark@interfaceschool.com>
2015-07-18 01:55:51 -0500

    Enter FanDuel game number and get eligible players

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,24 @@
 class PagesController < ApplicationController
 
   def fanduel
+    Batter.get_lineups
+    Pitcher.update_all selected:false
+    Batter.update_all selected:false
+    Batter.get_fd_data params[:fgn] unless params[:fgn].blank?
+    if params[:pitcher_ids] && params[:batter_ids]
+      @pitchers = Pitcher.find(params[:pitcher_ids])
+      @batters = Batter.find(params[:batter_ids])
+      @pitchers.each do |p|
+        p.selected = true
+        p.save
+      end
+      @batters.each do |b|
+        b.selected = true
+        b.save
+      end
+      lineup = Batter.optimal_fd
+      flash[:alert] = lineup
+    end
     @catchers = Batter.catchers_sorted_by_adj_fd_pts_per_1000_dollars
     @firstbasemen = Batter.firstbasemen_sorted_by_adj_fd_pts_per_1000_dollars
     @secondbasemen = Batter.secondbasemen_sorted_by_adj_fd_pts_per_1000_dollars
@@ -8,24 +26,6 @@ class PagesController < ApplicationController
     @shortstops = Batter.shortstops_sorted_by_adj_fd_pts_per_1000_dollars
     @outfielders = Batter.outfielders_sorted_by_adj_fd_pts_per_1000_dollars
     @pitchers = Pitcher.sorted_by_zips_fd_pts_per_1000_dollars
-  end
-
-  def update
-    Pitcher.update_all selected:false
-    Batter.update_all selected:false
-    @pitchers = Pitcher.find(params[:pitcher_ids])
-    @batters = Batter.find(params[:batter_ids])
-    @pitchers.each do |p|
-      p.selected = true
-      p.save
-    end
-    @batters.each do |b|
-      b.selected = true
-      b.save
-    end
-    lineup = Batter.optimal_fd
-    flash[:alert] = lineup
-    redirect_to root_path
   end
 
   def lines
