cb23010b46bf2d3890066198d9de8f9d4c47a84a
markmohr15 <mark@interfaceschool.com>
2015-07-20 15:35:49 -0500

    Change User_Line redirect to root

--- app/controllers/user_lines_controller.rb
+++ app/controllers/user_lines_controller.rb
@@ -3,7 +3,7 @@ class UserLinesController < ApplicationController
 
   def create
     @user_lines = params[:user_line].map { |attrs| UserLine.create(attrs.to_hash.merge(user_id: current_user.id)) unless attrs["visitor_off"] == "" && attrs["visitor_def"] == "" && attrs["home_off"] == "" && attrs["home_def"] == "" }
-    redirect_to lines_path
+    redirect_to root_path
   end
 
 end
